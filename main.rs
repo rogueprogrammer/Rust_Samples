48953df1639fa433f0ae07471840878abdf4215a
rogueprogrammer <dkhanna@uwaterloo.ca>
2024-04-28 18:35:58 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 fn main() {
 
     // CREATE NEW PROJECT - `cargo new <project_name>`
@@ -55,5 +56,53 @@ fn main() {
     let kabir = Person::new(String::from("Kabir"), 1);
     println!("Kabir's name: {}", kabir.get_name());
 
+    // HASHMAPS:
+    let mut map = HashMap::new();
+    map.insert("Zinnia", 31);
+    map.insert("Kabir", 1);
+    map.insert("Dev", 34);
+    if map.contains_key("Zinnia"){
+        let age = map.get("Zinnia").unwrap_or(&-1); // Default value if key is not found
+        println!("Found Zinnia! Age={}", age );
+        let age2 = map.get("suzy").unwrap_or(&-1); // Default value if key is not found
+        println!("suzy Age={}", age2 ); //prints -1
+    }
+    //iterate over the hashmap:
+    // NOTE: Use &map, instead of map since rust requires us to "borrow" the hashmap instead of 
+    // providing ownership. If & is not provided, then ownership transferred to for-loop and the map can't be used after loop
+    for(key, val) in &map {
+        println!("Key: {} Val: {}", key, val);
+    }
+    
+    // ITERATORS
+    let weather_types = vec!["sunny", "rainy", "snowy"];
+    for weather_type in weather_types.iter() {
+        match weather_type{
+            &"sunny" => println!("Summer time!"),
+            &"rainy" => println!("Spring time!"),
+            &"snowy" => println!("Winter time!"),
+            _ => {} // handle other types
+        }
+    }
+
+    // SMART POINTERS (BOX)
+    let new_arr = Box::new([1, 2, 3]);
+    // dereference the pointer to access the numbers
+    let numbers = *new_arr;
+    println!("First number: {}", numbers[0]);
+    println!("Second number: {}", numbers[1]);
+    println!("Third number: {}", numbers[2]);
+    
+    // CRATE AS A LIB - https://doc.rust-lang.org/rust-by-example/crates/using_lib.html
+    // 1. Create new file (rary.rs)
+    // 2. Compile the file using: `rustc --crate-type=lib rary.rs` you will see a new lib file created
+    // 3. Use the functions from the lib: rary::<function_name>
+    // 4. Compile and Create executable for the main executable linking to the lib: `rustc main.rs --extern rary=library.rlib`
+    // 5. run the executable now instead of `cargo run`: ./main.exe 
+    rary::public_function();
+
+    // CARGO PACKAGE MANAGEMENT - https://doc.rust-lang.org/rust-by-example/cargo/deps.html
+    // CARGO TESTS - https://doc.rust-lang.org/rust-by-example/cargo/test.html
+    
     println!("Hello, world!");
 }
